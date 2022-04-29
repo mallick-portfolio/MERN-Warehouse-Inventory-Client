707f9b90666b154a7a9126362cd0100583dd43f8
mallick-portfolio <tamal@gmail.com>
2022-04-29 13:30:56 +0600

    added social login functionality

--- src/components/shared/Social/Social.jsx
+++ src/components/shared/Social/Social.jsx
@@ -1,24 +1,34 @@
 import React, { useEffect } from "react";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
-import { BsFacebook, BsGithub,BsGoogle } from "react-icons/bs";
+import {
+  useSignInWithFacebook,
+  useSignInWithGithub,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import { BsFacebook, BsGithub, BsGoogle } from "react-icons/bs";
 import auth from "../../../firebase.init.js";
-import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router-dom';
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 import Loading from "../Loading/Loading.jsx";
 
 const Social = () => {
-  const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
-  const navigate = useNavigate()
+  const [signInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+  const [signInWithGithub, githubUser, githubLoading, githubError] =
+    useSignInWithGithub(auth);
+    const [signInWithFacebook, facebookUser, facebookLoading, facebookError] = useSignInWithFacebook(auth);
+
+
+  const navigate = useNavigate();
   useEffect(() => {
-    if (googleError) {
+    if (googleError || githubError || facebookError) {
       toast("Your signup attemp failed. Please try again");
-      navigate("/register");
+      // navigate("/register");
     }
-  }, [googleError, navigate]);
-  if (googleLoading) {
+  }, [facebookError, githubError, googleError, navigate]);
+  if (googleLoading || githubLoading || facebookLoading) {
     return <Loading />;
   }
-  if (googleUser) {
+  if (googleUser || githubUser || facebookUser) {
     return (
       <div>
         <p>Registered User: {googleUser.email}</p>
@@ -27,13 +37,21 @@ const Social = () => {
   }
   return (
     <div className="flex items-center justify-center my-4">
-      <button type="button" className="facebook-btn">
+      <button onClick={()=> signInWithFacebook()} type="button" className="facebook-btn">
         <BsFacebook />
       </button>
-      <button type="button" className="github-btn">
+      <button
+        onClick={() => signInWithGithub()}
+        type="button"
+        className="github-btn"
+      >
         <BsGithub />
       </button>
-      <button onClick={()=> signInWithGoogle()} type="button" className="google-btn">
+      <button
+        onClick={() => signInWithGoogle()}
+        type="button"
+        className="google-btn"
+      >
         <BsGoogle />
       </button>
     </div>
